ea62aaf011019f0a39b5c06f4238eacb270d19b4
Сергей <sergowindow@yandex.ru>
2025-04-02 22:48:19 +0300

    added methods

--- src/Author.java
+++ src/Author.java
@@ -1,3 +1,5 @@
+import java.util.Objects;
+
 public class Author {
     private final String name;
     private final String lastName;
@@ -18,5 +20,18 @@ public class Author {
     @Override
     public String toString() {
         return name + " " + lastName;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass())
+            return false;
+        Author author = (Author) o;
+        return Objects.equals(name, author.name) && Objects.equals(lastName, author.lastName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, lastName);
     }
 }
